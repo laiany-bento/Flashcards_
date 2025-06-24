42342cf7894f35bcde421ba86ab771caefcb6b91
laiany-bento <laiany.bento@escola.pr.gov.br>
2025-06-24 20:35:04 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Brasil República',
+    'O que foi a política café com leite?',
+    'A aliança entre São Paulo (café) e Minas Gerais'
+)
+
+criaCartao(
+    'BrasilRepública,
+    'Em que ano o Brasil se tornou uma república?',
+    '15 de novembro de 1889'
+)
+
+criaCartao(
+    'BrasilRepública,
+    'Quem proclamoua República no Brasil?',
+    'Deodoro da Fonseca'
+)
+
+criaCartao(
+    'Brasil República,
+    'O que foi o voto de Cabresto?',
+    'Voto conduzido por alguém'
+)
+
+criaCartao(
+    'Brasil República,
+    'Qual foi o principal produto de exportação do Brasil durante a República Velha?',
+    'Café'
+)
+
+criaCartao(
+    'Brasil República,
+    'Quem foi o líder da era Vargas?',
+    'Getúlio Vargas'
+)
